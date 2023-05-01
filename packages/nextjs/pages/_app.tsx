ff1ef5a578a9df9d7b3fe967ff9c7bfa6202c353
Carlos Sánchez <oceanrdn@gmail.com>
2023-05-01 13:53:02 +0200

    Footer Icon. Clean tailwind theme

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -41,12 +41,12 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
         avatar={BlockieAvatar}
         theme={isDarkTheme ? darkTheme() : lightTheme()}
       >
-        <div className="flex flex-col min-h-screen">
+        <div>
           {scaffoldConfig.hideHeader ? "" : <Header />}
           <main className="relative flex flex-col flex-1">
             <Component {...pageProps} />
           </main>
-          {scaffoldConfig.hideHeader ? "" : <Footer />}
+          <Footer />
         </div>
         <Toaster />
       </RainbowKitProvider>
