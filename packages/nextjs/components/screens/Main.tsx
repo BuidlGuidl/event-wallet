bf93b32a5b9e11f7b2a93d7108497e4656a420dd
Carlos Sánchez <oceanrdn@gmail.com>
2023-05-19 17:39:25 +0200

    Welcome text

--- packages/nextjs/components/screens/Main.tsx
+++ packages/nextjs/components/screens/Main.tsx
@@ -1,10 +1,16 @@
 /**
- * Send Screen
+ * Main Screen
  */
 export const Main = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center my-16 font-bold">Welcome to the Event</div>
+      <div className="flex flex-col gap-2 max-w-[300px] text-center">
+        <p className="font-bold mb-0">Welcome to EDCON!</p>
+        <p>
+          The <span className="font-bold">EDCON wallet</span> will help you collect soulbound NFTs of each speaker. You
+          can also collect and trade 💎 gems, a pop up currency for the event.
+        </p>
+      </div>
     </>
   );
 };
