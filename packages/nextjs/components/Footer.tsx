74725579a0f7446133ec53c4aa6f8a35238e3428
Carlos Sánchez <oceanrdn@gmail.com>
2023-05-19 18:31:36 +0200

    Footer

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -15,11 +15,17 @@ export const Footer = () => {
   }
 
   return (
-    <button
-      className="fixed bg-primary inline-block p-4 rounded-full bottom-0 mb-8 left-0 right-0 w-[72px] m-auto"
-      onClick={() => setIsQrReaderOpen(true)}
-    >
-      <ScanIcon width="2.5rem" height="2.5rem" className="text-white" />
-    </button>
+    <>
+      <button
+        className="fixed bg-primary inline-block p-4 rounded-full bottom-0 mb-8 left-0 right-0 w-[72px] m-auto"
+        onClick={() => setIsQrReaderOpen(true)}
+      >
+        <ScanIcon width="2.5rem" height="2.5rem" className="text-white" />
+      </button>
+      <div className="fixed bottom-0 flex gap-1 justify-center w-full text-xs mb-2 text-gray-400">
+        <span className="italic">powered by</span>
+        <span>Gateway.fm</span>
+      </div>
+    </>
   );
 };
