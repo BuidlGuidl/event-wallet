caeb043c91bac94aaef30a5619b40945db0c9ae5
Carlos Sánchez <oceanrdn@gmail.com>
2023-07-03 12:41:45 +0200

    Fix middleware

--- packages/nextjs/middleware.ts
+++ packages/nextjs/middleware.ts
@@ -5,8 +5,10 @@ export const config = {
   matcher: "/api/:function*",
 };
 
-export function middleware() {
-  if (!Object.keys(kv).length) {
+export async function middleware() {
+  try {
+    await kv.ping();
+  } catch (error) {
     return new NextResponse(JSON.stringify({ error: "KV storage is not set" }), {
       status: 401,
       headers: { "content-type": "application/json" },
