338cc4c274704ffbf2d8eb3c29979eb38d457d23
Angry Coding <mailidanielifechukwu@gmail.com>
2023-10-12 23:38:16 +0100

    chore: Removed scroll

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -98,7 +98,7 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="flex flex-col items-center justify-center py-2">
-        <div className="md:min-w-[32rem] p-6 my-14 w-full md:w-[35%]">
+        <div className="md:min-w-[32rem] p-6 my-14 md:my-10 w-full md:w-[35%]">
           <Image src="/bg.svg" alt="Event Wallet Logo" width={21} height={28} className="absolute top-0 left-0 m-5" />
           <div className="absolute top-0 right-0 m-5">
             <div className="flex items-center">
@@ -175,7 +175,7 @@ const Home: NextPage = () => {
             </>
           </div>
 
-          <div className="md:static fixed bottom-0 left-0 w-full pb-4 bg-white md:bg-[#ffffff00] h-[50vh] md:h-full overflow-y-scroll rounded-t-3xl p-4">
+          <div className="md:static fixed bottom-0 left-0 w-full pb-4 bg-white md:bg-[#ffffff00] h-[50vh] md:h-full overflow-y-scroll rounded-t-3xl p-2 no-scrollbar">
             {screenRender}
           </div>
         </div>
