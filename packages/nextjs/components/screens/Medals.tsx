b30ccf512abafcff5aa94bbdc0f8c47b5dcfeafb
Damian <damianmarti@gmail.com>
2023-10-19 18:00:31 -0300

    Add token leaderboard to medals screen

--- packages/nextjs/components/screens/Medals.tsx
+++ packages/nextjs/components/screens/Medals.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { TokenLeaderboard } from "~~/components/TokenLeaderboard";
 
 export default function Medals() {
   return (
-    <div className="flex flex-col gap-2 text-center m-auto overflow-x-hidden">
+    <div className="flex flex-col place-items-center gap-2 text-center m-auto overflow-x-hidden">
       <div className="flex flex-col gap-2">
-        <h1 className="font-medium text-xl"> Leaderboard </h1>
+        <h1 className="font-medium text-xl">Leaderboard</h1>
       </div>
+      <TokenLeaderboard />
     </div>
   );
 }
